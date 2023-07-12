cbc45432bfc48c837adcd3ef4979abceb872c287
tnp2004 <iceandiceisice2004@gmail.com>
2023-07-12 19:00:15 +0700

    Update: tokens to alternative and readme

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # HOW TO USE
 1. replace your tokens, owner, repo and timezone(line 59)
 2. open the terminal and run "go build"
-3. change .exe file name to whatever you want to use when you use it in the terminal
+3. change the name of .exe file to whatever you want to use when you use it in the terminal
+4. add .exe path to environment variables
--- main.go
+++ main.go
@@ -23,16 +23,18 @@ type Commit struct {
 }
 
 func main() {
-	tokens := "{github personal access tokens}"
+	// github tokens for access to private repo (alternative)
+	tokens := ""
+
+	// github account owner
+	owner := "tnp2004"
+	// The repository that you want to watch commits
+	repo := "github-report-cli"
 
 	spinner := spinner.New(spinner.CharSets[11], 100*time.Millisecond)
 	spinner.Suffix = " fetching..."
 	spinner.Start()
-
-	// github account owner
-	owner := "{default github owner}"
-	// The repository that you want to watch commits
-	repo := "{default github repo}"
+	fmt.Println("")
 
 	argLen := len(os.Args[1:])
 	if argLen == 1 {
@@ -48,11 +50,12 @@ func main() {
 
 	var commits []Commit
 	queryCommits(tokens, owner, repo, &commits)
-	printCommits(commits)
+	printCommits(commits, owner, repo)
 	spinner.Stop()
 }
 
-func printCommits(commits []Commit) {
+func printCommits(commits []Commit, owner, repo string) {
+	fmt.Printf("\n\n*** %s: %s ***\n", owner, repo)
 	for i := len(commits) - 1; i >= 0; i-- {
 		commitTimeUTC, _ := time.Parse(time.RFC3339, commits[i].CommitInfo.Committer.Date)
 		// chage to your time zone
@@ -77,27 +80,36 @@ func queryCommits(tokens, owner, repo string, commits *[]Commit) {
 	endpoint := fmt.Sprintf("https://api.github.com/repos/%s/%s/commits", owner, repo)
 	req, err := http.NewRequest("GET", endpoint, nil)
 	if err != nil {
-		panic(err)
+		fmt.Println(err)
 	}
-	req.Header = http.Header{
-		"Authorization": {"Bearer " + tokens},
+
+	var res *http.Response
+	if len(tokens) != 0 {
+		req.Header = http.Header{
+			"Authorization": {"Bearer " + tokens},
+		}
 	}
-	res, err := client.Do(req)
+
+	res, err = client.Do(req)
+
 	if err != nil {
-		panic(err)
+		fmt.Println("Something went wrong")
 	}
+
 	defer res.Body.Close()
 
 	if res.StatusCode != 200 {
-		panic("Github API not available")
+		fmt.Println("\nRepository not found")
+		return
 	}
 
 	body, err := io.ReadAll(res.Body)
+
 	if err != nil {
-		panic(err)
+		fmt.Println(err)
 	}
 	err = json.Unmarshal(body, commits)
 	if err != nil {
-		panic(err)
+		fmt.Println(err)
 	}
 }
